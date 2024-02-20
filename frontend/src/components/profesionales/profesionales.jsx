b8ec1d8d08543bbd7744830af3ceb7981fdc6856
Fernando Lorenzo <fernando.a.lorenzo@gmail.com>
2024-02-20 00:32:41 -0300

    Avances tabla 2024-02-20

--- frontend/src/components/profesionales/profesionales.jsx
+++ frontend/src/components/profesionales/profesionales.jsx
@@ -1,32 +1,44 @@
-import React, { useState, useEffect } from "react";
-import { useTable, useSortBy, usePagination, useFilters } from "react-table";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { useState, useEffect, useMemo } from "react";
+import { useTable, useSortBy, usePagination } from "react-table";
 
 const ProfesionalesTabla = () => {
-	const [data, setData] = useState([]);
-	const [loading, setLoading] = useState(true);
+    const [data, setData] = useState([]);
+	const [filterText, setFilterText] = useState("");
+	const [selectedPageSize, setSelectedPageSize] = useState(10);
 
 	useEffect(() => {
-		fetchData();
+		const fetchProfesionales = async () => {
+			try {
+				const response = await fetch(
+					"http://localhost:5000/profesionales"
+				);
+				if (!response.ok) {
+					throw new Error("Error al obtener los datos");
+				}
+				const data = await response.json();
+				setData(data.data);
+			} catch (error) {
+				console.error("Error:", error.message);
+			}
+		};
+
+		fetchProfesionales();
 	}, []);
 
-	const fetchData = async () => {
-		try {
-			const response = await fetch("http://localhost:5000/profesionales");
-			if (!response.ok) {
-				throw new Error("Error al obtener datos");
-			}
-			const jsonData = await response.json();
-			if (!Array.isArray(jsonData)) {
-				throw new Error("Los datos recibidos no son un array");
-			}
-			setData(jsonData);
-			setLoading(false);
-		} catch (error) {
-			console.error("Error:", error.message);
-			setLoading(false);
-		}
-	};
+	const filteredData = useMemo(() => {
+		if (!filterText) return data;
+		return data.filter((row) =>
+			Object.entries(row).some(
+				([key, cellValue]) =>
+					key !== "id" &&
+					cellValue &&
+					cellValue
+						.toString()
+						.toLowerCase()
+						.includes(filterText.toLowerCase())
+			)
+		);
+	}, [data, filterText]);
 
 	const columns = React.useMemo(
 		() => [
@@ -52,15 +64,25 @@ const ProfesionalesTabla = () => {
 			},
 			{
 				Header: "Acciones",
-				Cell: () => (
-					<>
-						<button className="btn btn-primary btn-sm mr-2">
+				accessor: "id",
+				Cell: ({ row }) => (
+					<div>
+						<button
+							className="btn btn-warning mx-2 btn-sm"
+							data-bs-id="{user.id_user}"
+							onClick={() => editarProfesional(row.original)}>
+							<i className="fa-regular fa-pen-to-square"></i>{" "}
 							Editar
 						</button>
-						<button className="btn btn-danger btn-sm">
-							Eliminar
+						<button
+							className="btn btn-danger mx-2 btn-sm"
+							data-bs-id="{user.id_user}"
+							onClick={() =>
+								eliminarProfesional(row.original.id)
+							}>
+							<i className="fa-regular fa-trash-can"></i> Eliminar
 						</button>
-					</>
+					</div>
 				),
 			},
 		],
@@ -76,122 +98,227 @@ const ProfesionalesTabla = () => {
 		canPreviousPage,
 		canNextPage,
 		pageOptions,
-		pageCount,
 		gotoPage,
 		nextPage,
 		previousPage,
-		setPageSize,
+		pageCount,
 		state: { pageIndex, pageSize },
+		setPageSize: updatePageSize,
 	} = useTable(
 		{
 			columns,
-			data,
-			initialState: { pageIndex: 0, pageSize: 5 },
+			data: filteredData,
+			initialState: { pageIndex: 0, pageSize: selectedPageSize }, // Usar selectedPageSize
+			autoResetPage: false,
+			autoResetPageSize: false,
 		},
-		useFilters,
 		useSortBy,
 		usePagination
 	);
 
-	if (loading) return <div>Loading...</div>;
+	const handleFilterChange = (e) => {
+		const value = e.target.value || "";
+		setFilterText(value);
+	};
+
+	const handleClearFilter = () => {
+		setFilterText("");
+	};
+	
+	const handlePageSizeChange = (e) => {
+		const size = parseInt(e.target.value, 10);
+		setSelectedPageSize(size);
+		gotoPage(0);
+		updatePageSize(size); // Usar updatePageSize
+	};
 
 	return (
-		<div>
-			<input
-				type="text"
-				placeholder="Buscar..."
-				className="form-control mb-3"
-				onChange={(e) => {
-					// Implementar la lógica de filtrado
-				}}
-			/>
-			<table {...getTableProps()} className="table">
-				<thead>
-					{headerGroups.map((headerGroup) => (
-						<tr {...headerGroup.getHeaderGroupProps()}>
-							{headerGroup.headers.map((column) => (
-								<th
-									{...column.getHeaderProps(
-										column.getSortByToggleProps()
-									)}>
-									{column.render("Header")}
-									<span>
-										{column.isSorted
-											? column.isSortedDesc
-												? " 🔽"
-												: " 🔼"
-											: ""}
-									</span>
-								</th>
-							))}
-						</tr>
-					))}
-				</thead>
-				<tbody {...getTableBodyProps()}>
-					{page.map((row) => {
-						prepareRow(row);
-						return (
-							<tr {...row.getRowProps()}>
-								{row.cells.map((cell) => {
+		<div className="content-wrapper">
+			<div className="content-header">
+				<div className="container-fluid">
+					<div className="row mb-2">
+						<div className="col-sm-6">
+							<h1 className="m-0">Profesionales</h1>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div>
+				<section className="content">
+					<div className="container-fluid">
+						<div className="d-grid gap-2 d-md-flex justify-content-md-end mb-3">
+							<div className="input-group col-4 me-md-2">
+								
+									<input
+										type="text"
+										className="form-control"
+										placeholder="Filtrar..."
+										value={filterText}
+										onChange={handleFilterChange}
+								/>
+								{filterText && (
+									<div className="input-group-append">
+									<button
+										className="btn bg-white"
+										title="Limpiar búsqueda"
+										type="button"
+										onClick={() => setFilterText("")}>
+										<i className="fa-regular fa-circle-xmark"></i>
+									</button>
+								</div>
+    
+									)}
+								
+							</div>
+							<button
+								type="button"
+								className="btn btn-primary"
+								id="abrirModalAgregar"
+								onClick={() => handleAgregar()}>
+								<i className="fa-regular fa-square-plus"></i>{" "}
+								Agregar
+							</button>
+						</div>
+						<table
+							{...getTableProps()}
+							className="table table-hover table-striped table-responsive-sm table-sm table-borderless align-middle">
+							<thead>
+								{headerGroups.map((headerGroup) => (
+									<tr {...headerGroup.getHeaderGroupProps()}>
+										{headerGroup.headers.map((column) => (
+											<th
+												{...column.getHeaderProps(
+													column.getSortByToggleProps()
+												)}>
+												{column.render("Header")}
+												<span>
+													{column.isSorted
+														? column.isSortedDesc
+															? " 🔽"
+															: " 🔼"
+														: ""}
+												</span>
+											</th>
+										))}
+									</tr>
+								))}
+							</thead>
+							<tbody {...getTableBodyProps()}>
+								{page.map((row) => {
+									prepareRow(row);
 									return (
-										<td {...cell.getCellProps()}>
-											{cell.render("Cell")}
-										</td>
+										<tr {...row.getRowProps()}>
+											{row.cells.map((cell) => {
+												return (
+													<td
+														{...cell.getCellProps()}>
+														{cell.render("Cell")}
+													</td>
+												);
+											})}
+										</tr>
 									);
 								})}
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
-			<div className="pagination">
-				<button onClick={() => gotoPage(0)} disabled={!canPreviousPage}>
-					{"<<"}
-				</button>{" "}
-				<button
-					onClick={() => previousPage()}
-					disabled={!canPreviousPage}>
-					{"<"}
-				</button>{" "}
-				<button onClick={() => nextPage()} disabled={!canNextPage}>
-					{">"}
-				</button>{" "}
-				<button
-					onClick={() => gotoPage(pageCount - 1)}
-					disabled={!canNextPage}>
-					{">>"}
-				</button>{" "}
-				<span>
-					Página{" "}
-					<strong>
-						{pageIndex + 1} de {pageOptions.length}
-					</strong>{" "}
-				</span>
-				<span>
-					| Ir a la página:{" "}
-					<input
-						type="number"
-						defaultValue={pageIndex + 1}
-						onChange={(e) => {
-							const page = e.target.value
-								? Number(e.target.value) - 1
-								: 0;
-							gotoPage(page);
-						}}
-						style={{ width: "100px" }}
-					/>
-				</span>{" "}
-				<select
-					value={pageSize}
-					onChange={(e) => {
-						setPageSize(Number(e.target.value));
-					}}>
-					{[5, 10, 25, 100].map((pageSize) => (
-						<option key={pageSize} value={pageSize}>
-							Mostrar {pageSize}
-						</option>
-					))}
-				</select>
+							</tbody>
+						</table>
+						<div className="container-fluid my-2">
+							<div className="row justify-content-between">
+								<div className="col col-4 d-flex justify-content-start pagination">
+									<button
+										className="btn btn-sm btn-outline-primary ms-1"
+										onClick={() => gotoPage(0)}
+										disabled={!canPreviousPage}>
+										<i className="fa-solid fa-backward-step"></i>
+									</button>{" "}
+									<button
+										className="btn btn-sm btn-outline-primary ms-1"
+										onClick={() => previousPage()}
+										disabled={!canPreviousPage}>
+										<i className="fa-solid fa-caret-left"></i>
+									</button>{" "}
+									<button
+										className="btn btn-sm btn-outline-primary ms-1"
+										onClick={() => nextPage()}
+										disabled={!canNextPage}>
+										<i className="fa-solid fa-caret-right"></i>
+									</button>{" "}
+									<button
+										className="btn btn-sm btn-outline-primary ms-1"
+										onClick={() => gotoPage(pageCount - 1)}
+										disabled={!canNextPage}>
+										<i className="fa-solid fa-forward-step"></i>
+									</button>{" "}
+								</div>
+								<div className="col col-4 d-flex justify-content-evenly pagination content-align-center">
+									<span>
+										Página <strong>{pageIndex + 1}</strong>{" "}
+										de <strong>{pageOptions.length}</strong>
+									</span>
+									<span>Ir a la página: </span>
+									<input
+										className="form-control form-control-sm"
+										type="number"
+										defaultValue={pageIndex + 1}
+										onChange={(e) => {
+											const page = e.target.value
+												? Number(e.target.value) - 1
+												: 0;
+											gotoPage(page);
+										}}
+										style={{ width: "4rem" }}
+									/>
+								</div>
+								<div className="col col-4 d-flex justify-content-end pagination">
+									<select
+										className="form-select form-select-sm"
+										value={selectedPageSize}
+										onChange={handlePageSizeChange}>
+										{[10, 25, 50, 100].map((size) => (
+											<option key={size} value={size}>
+												{size}
+											</option>
+										))}
+									</select>
+									{/* <div className="col col-4 d-flex justify-content-evenly pagination">
+									<span>
+										Página <strong>{pageIndex + 1}</strong>{" "}
+										de <strong>{pageOptions.length}</strong>
+									</span>
+									Ir a la página:{" "}
+									<input
+										className="form-control form-control-sm"
+										type="number"
+										defaultValue={pageIndex + 1}
+										onChange={(e) => {
+											const page = e.target.value
+												? Number(e.target.value) - 1
+												: 0;
+											gotoPage(page);
+										}}
+										style={{ width: "4rem" }}
+									/>
+								</div>
+								<div className="col col-4 d-flex justify-content-end pagination">
+									<select
+										className="form-select form-select-sm"
+										value={pageSize}
+										onChange={(e) => {
+											setPageSize(Number(e.target.value));
+										}}>
+										{[5, 10, 25, 100].map((pageSize) => (
+											<option
+												key={pageSize}
+												value={pageSize}>
+												Mostrar {pageSize}
+											</option>
+										))}
+									</select>
+								</div> */}
+								</div>
+							</div>
+						</div>
+					</div>
+				</section>
 			</div>
 		</div>
 	);
--- frontend/src/components/profesionales/prueba.jsx
+++ frontend/src/components/profesionales/prueba.jsx
@@ -1,252 +1,114 @@
-// En ProfesionalesTabla.jsx
-import React, { useMemo, useState } from "react";
-import {
-	useTable,
-	useSortBy,
-	useGlobalFilter,
-	usePagination,
-} from "react-table";
+import { useMemo } from "react";
+import { useTable } from "react-table";
+import useRows from "./hooks/useRows";
+import useColumns from "./hooks/useColumns";
 
-const ProfesionalesTabla = ({ profesionales }) => {
-	const [data, setData] = useState(profesionales);
+const columns = useColumns();
+const data = useRows();
+const table = useTable({ columns, data });
 
-	const columns = useMemo(
+const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } =
+	table;
+
+export default function useRows() {
+	const rows = useMemo(
 		() => [
-			{ Header: "Nombre", accessor: "nombre" },
-			{ Header: "DNI", accessor: "dni", style: "text-center" },
-			{ Header: "CUIT", accessor: "cuit" },
-			{ Header: "Telefono", accessor: "telefono" },
-			{ Header: "e-Mail", accessor: "email" },
 			{
-				Header: "Activo",
-				accessor: "activo",
-				Cell: ({ value }) => (
-					<input type="checkbox" checked={value} readOnly />
-				),
+				marca: "Audi",
+				modelo: "A3",
+				segmento: "Sedan, Convertible",
+				anio: "2015",
 			},
 			{
-				Header: "Acciones",
-				accessor: "id",
-				Cell: ({ row }) => (
-					<div>
-						<button
-							className="btn btn-warning mx-2 btn-sm"
-							data-bs-id="{user.id_user}"
-							onClick={() => editarProfesional(row.original)}>
-							<i className="fa-regular fa-pen-to-square"></i>{" "}
-							Editar
-						</button>
-						<button
-							className="btn btn-danger mx-2 btn-sm"
-							data-bs-id="{user.id_user}"
-							onClick={() =>
-								eliminarProfesional(row.original.id)
-							}>
-							<i className="fa-regular fa-trash-can"></i> Eliminar
-						</button>
-					</div>
-				),
+				marca: "Audi",
+				modelo: "A3",
+				segmento: "Wagon",
+				anio: "2013",
+			},
+			{
+				marca: "Audi",
+				modelo: "A3 Sportback e-tron",
+				segmento: "Wagon",
+				anio: "2016",
+			},
+			{
+				marca: "Audi",
+				modelo: "A4",
+				segmento: "Sedan, Convertible",
+				anio: "2006",
+			},
+			{
+				marca: "Audi",
+				modelo: "A4",
+				segmento: "Sedan, Wagon",
+				anio: "2001",
+			},
+			{
+				marca: "Audi",
+				modelo: "A4 allroad",
+				segmento: "Wagon",
+				anio: "2019",
+			},
+			{
+				marca: "Audi",
+				modelo: "A5",
+				segmento: "Coupe",
+				anio: "2008",
+			},
+			{
+				marca: "Audi",
+				modelo: "A5 Sport",
+				segmento: "Convertible, Coupe",
+				anio: "2017",
+			},
+			{
+				marca: "Audi",
+				modelo: "Q3",
+				segmento: "SUV",
+				anio: "2020",
+			},
+			{
+				marca: "Audi",
+				modelo: "R8",
+				segmento: "Coupe",
+				anio: "2008",
+			},
+			{
+				marca: "Audi",
+				modelo: "TT",
+				segmento: "Coupe",
+				anio: "2019",
+			},
+			{
+				marca: "Audi",
+				modelo: "Q7",
+				segmento: "SUV",
+				anio: "2015",
+			},
+			{
+				marca: "Audi",
+				modelo: "Q8",
+				segmento: "SUV",
+				anio: "2019",
+			},
+			{
+				marca: "Audi",
+				modelo: "Cabriolet",
+				segmento: "Convertible, Coupe",
+				anio: "1996",
 			},
 		],
 		[]
 	);
 
-	const {
-		getTableProps,
-		getTableBodyProps,
-		headerGroups,
-		rows,
-		prepareRow,
-		state: { globalFilter, pageIndex, pageSize },
-		setGlobalFilter,
-		page,
-		nextPage,
-		previousPage,
-		canPreviousPage,
-		canNextPage,
-		pageOptions,
-		gotoPage,
-		pageCount,
-		setPageSize,
-	} = useTable(
-		{
-			columns,
-			data,
-			initialState: { pageSize: 5, pageIndex: 0 },
-		},
-		useGlobalFilter,
-		useSortBy,
-		usePagination
-	);
+	return rows;
+}
 
-	return (
-		<div className="content-wrapper">
-			<div className="content-header">
-				<div className="container-fluid">
-					<div className="row mb-2">
-						<div className="col-sm-6">
-							<h1 className="m-0">Profesionales</h1>
-						</div>
-					</div>
-				</div>
-			</div>
-			<section className="content">
-				<div className="container-fluid">
-					<div className="d-grid gap-2 d-md-flex justify-content-md-end mb-3">
-						<div className="input-group  col-4 me-md-2">
-							<input
-								value={globalFilter || ""}
-								onChange={(e) =>
-									setGlobalFilter(e.target.value)
-								}
-								placeholder="Buscar profesionales..."
-								className="form-control"
-							/>
-							{globalFilter && (
-								<div className="input-group-append">
-									<button
-										className="btn bg-white"
-										title="Limpiar búsqueda"
-										type="button"
-										onClick={() => setGlobalFilter("")}>
-										<i className="fa-regular fa-circle-xmark"></i>
-									</button>
-								</div>
-							)}
-						</div>
-						<button
-							type="button"
-							className="btn btn-primary"
-							id="abrirModalAgregar"
-							onClick={() => handleAgregar()}>
-							<i className="fa-regular fa-square-plus"></i>{" "}
-							Agregar
-						</button>
-					</div>
-					<div>
-						<table
-							{...getTableProps()}
-							className="table table-hover table-striped table-responsive-sm table-sm table-borderless align-middle">
-							<thead className="table-dark align-middle">
-								{headerGroups.map((headerGroup) => (
-									<tr {...headerGroup.getHeaderGroupProps()}>
-										{headerGroup.headers.map((column) => (
-											<th
-												{...column.getHeaderProps(
-													column.getSortByToggleProps()
-												)}>
-												{column.render("Header")}
-												<span>
-													{column.isSorted
-														? column.isSortedDesc
-															? " ↓"
-															: " ↑"
-														: ""}
-												</span>
-											</th>
-										))}
-									</tr>
-								))}
-							</thead>
-							<tbody {...getTableBodyProps()}>
-								{page.map((row) => {
-									prepareRow(row);
-									return (
-										<tr {...row.getRowProps()}>
-											{row.cells.map((cell) => (
-												<td {...cell.getCellProps()}>
-													{cell.render("Cell")}
-												</td>
-											))}
-										</tr>
-									);
-								})}
-							</tbody>
-						</table>
-					</div>
-					<div className="container-fluid">
-						<div className="row justify-content-between">
-							<div className="col-6">
-								Mostrando {pageIndex * pageSize + 1} -{" "}
-								{pageIndex * pageSize + page.length} de{" "}
-								{data.length} registros
-							</div>
-							<div className="col-4 justify-content-between">
-								<div className="row">
-									<div className="col">
-										<select
-											id="items"
-											className="form-select form-select-sm"
-											value={pageSize}
-											onChange={(e) => {
-												setPageSize(
-													Number(e.target.value)
-												);
-												const newData =
-													profesionales.slice(
-														pageIndex *
-															e.target.value,
-														(pageIndex + 1) *
-															e.target.value
-													);
-												setData(newData);
-											}}>
-											{[5, 10, 25, 50, 100].map(
-												(pageSize) => (
-													<option
-														key={pageSize}
-														value={pageSize}>
-														{pageSize} por página
-													</option>
-												)
-											)}
-										</select>
-									</div>
-									<div className="col mx-1 d-flex justify-content-center">
-										<button
-											className="btn btn-sm btn-outline-secondary ms-1"
-											onClick={() => gotoPage(0)}
-											disabled={!canPreviousPage}>
-											<i className="fa-solid fa-backward-step"></i>
-										</button>
-										<button
-											className="btn btn-sm btn-outline-secondary ms-1"
-											onClick={() => previousPage()}
-											disabled={!canPreviousPage}>
-											<i className="fa-solid fa-caret-left"></i>
-										</button>
-										<button
-											className="btn btn-sm btn-outline-secondary ms-1"
-											onClick={() => nextPage()}
-											disabled={!canNextPage}>
-											<i className="fa-solid fa-caret-right"></i>
-										</button>
-										<button
-											className="btn btn-sm btn-outline-secondary ms-1"
-											onClick={() =>
-												gotoPage(pageCount - 1)
-											}
-											disabled={!canNextPage}>
-											<i className="fa-solid fa-forward-step"></i>
-										</button>
-									</div>
-									<div className="col">
-										<span>
-											Página{" "}
-											<strong>
-												{pageIndex + 1} de{" "}
-												{pageOptions.length}
-											</strong>{" "}
-										</span>
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</section>
-		</div>
-	);
-};
+import React from 'react'
 
-export default ProfesionalesTabla;
+export const prueba = () => {
+  return (
+	<div>prueba</div>
+  )
+}
+
